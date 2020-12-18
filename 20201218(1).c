c484091f1da2bdd8212f06c4a35e5979eed9488f
변승현 <67039023+gusanans218@users.noreply.github.com>
2020-12-19 00:14:06 +0900

    20201218

---
+++ 20201218(1).c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main() 
+{
+	//for (int i = 0; i < 100; i++)
+	//{
+	//	printf("hello world\n");
+	//}
+	//return 0;
+	////for문으로 hello world백번 출력하기
+
+	//int count;
+	//scanf_s("%d", &count);
+	//for (int i = 1; i < count+1; i++)
+	//{
+	//	printf("hello, world! %d\n", i);
+	//}
+	//return 0;
+	////hello world 입력한 숫자만큼 출력
+
+	//for (int i = 0, j = 0; i < 10; i++, j += 2)
+	//{
+	//	printf("i: %d, j : %d\n", i, j);
+	//}
+	//return 0;
+	////i는 1씩 증가, j는 2씩 증가
+
+
+}
